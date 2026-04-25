c500070703eb3e49d4bf78280c425ef73a771a35
rob <kousun12@gmail.com>
2026-04-25 11:26:06 -0400

    Initial Commit

---
+++ robios/robiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  robiosApp.swift
+//  robios
+//
+//  Created by rob on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct robiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
